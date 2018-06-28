bf7229ea3b0fa61d9346b462f2ce20b162e7184b
Maison Galvão <maisongalvao144@gmail.com>
2018-06-28 14:01:14 -0400

    Modificações

--- forca.c
+++ forca.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
+void aberturadojogo(){
+	printf("*******************************************************************\n");
+	printf("* 		***       JOGO   DE   FORCA        ***            *\n");
+	printf("*******************************************************************\n\n\n\n");
+}
+
 int main() {
-  int i,j,acertou=0,enforcou=0,ttentativas=10,tentativa=0,achou,cont=0;
+  int i,j,acertou=0,enforcou=0,totaltentativas=10,tentativa=0,achou,cont=0;
   char palavrasecreta[20],chute;
   char chutes[tentativa];
-  sprintf(palavrasecreta, "uber");
+  sprintf(palavrasecreta, "semtrad");
+  aberturadojogo();
   do{
-  	printf("\nQual e o seu palpite ?\n");
-  	scanf(" %c",&chute);
-  	chutes[tentativa]=chute;
-  	tentativa++;
-  		cont=0;
-  		for(i=0;i<strlen(palavrasecreta);i++){
-	  		achou = 0;
-	  		for(j=0;j<=tentativa;j++){
-	  			if(palavrasecreta[i]==chutes[j]){
-	  				achou=1;
-	  				break;
-				} 
-			}
+  	achou = 0;
+  	for(i=0;i<strlen(palavrasecreta);i++){
+	  	achou = 0;
+	  	for(j=0;j<tentativa;j++){
+	  		if(palavrasecreta[i]==chutes[j]){
+	  			achou=1;
+	  			break;
+			} 
+		}
 			if(achou){
-				printf("%c ", palavrasecreta[i]);
 				cont++;
+				printf("%c ", palavrasecreta[i]);
 			}else{
 				printf("_ ");
 			}
+	}
 			if(cont==strlen(palavrasecreta)){
 				printf("\nVoce descobriu a palavra secreta!!");
-				acertou=1;
+				return 0;
 			}
-	    }
 	    printf("\n");
-  }while(!acertou && !enforcou);
+	    printf("\nQual e o seu CHUTE ?\n");
+  		scanf(" %c",&chute);
+  		chutes[tentativa]=chute;
+  		tentativa++;
+  		cont=0;
+  		chutes[tentativa]=chute;
+	    
+  }while(!acertou && !enforcou && tentativa<totaltentativas);
   
   return 0;
 }
